02c9054a720fb73d79c96afe8bfbee7785b38220
april <aprilwang96@gmail.com>
2018-01-18 11:45:53 +0800

    commit

--- StationeryStoreInventorySystem/RequisitionDetailsEmployee.aspx.cs
+++ StationeryStoreInventorySystem/RequisitionDetailsEmployee.aspx.cs
@@ -15,18 +15,12 @@ public partial class RequisitionDetails : System.Web.UI.Page
     protected void Page_Load(object sender, EventArgs e)
     {
         id = Convert.ToInt32(Request.QueryString["id"]);
-        //int id = 24;
-        
-
-        r = ReqBS.getRequisition(id);
-        Label2.Text = r.RequestedBy.ToString();
-        Label3.Text = r.RequestDate.ToString();
-        Label4.Text = r.Status.ToString();
+        LoadData();
 
         if (!IsPostBack)
         {
             showAllItems();
-            if(r.Status=="Rejected" || r.Status=="Closed")
+            if (r.Status == "Rejected" || r.Status == "Closed")
             {
                 Cancel.Visible = false;
                 Add.Visible = false;
@@ -35,9 +29,19 @@ public partial class RequisitionDetails : System.Web.UI.Page
             DropDownList2.DataSource = ReqBS.getItem();
             DropDownList2.DataBind();
         }
-
         des = DropDownList2.SelectedItem.ToString();
         Label6.Text = ReqBS.getUOM(des);
+    }
+
+    public void LoadData()
+    {
+       
+        //int id = 24;
+
+        r = ReqBS.getRequisition(id);
+        Label2.Text = r.RequestedBy.ToString();
+        Label3.Text = r.RequestDate.ToString();
+        Label4.Text = r.Status.ToString();
     }
 
     protected void showAllItems()
@@ -79,7 +83,17 @@ public partial class RequisitionDetails : System.Web.UI.Page
     protected void Add_Click(object sender, EventArgs e)
     {
         Panel1.Visible = true;
-        
+        Add.Visible = false;
+        Close.Visible = true;
+
+    }
+
+    protected void Close_Click(object sender, EventArgs e)
+    {
+        Panel1.Visible = false;
+        Add.Visible = true;
+        Close.Visible = false;
+
     }
 
     protected void New_Click(object sender, EventArgs e)
@@ -93,6 +107,50 @@ public partial class RequisitionDetails : System.Web.UI.Page
 
         showAllItems();
     }
-}
 
+    protected void Delete_Click(object sender, EventArgs e)
+    {
+        //LoadData();
+        GridViewRow row = ((System.Web.UI.WebControls.Button)sender).Parent.Parent as GridViewRow;
+        string itemDes = GridView1.DataKeys[row.RowIndex].Value.ToString();
+
+
+        Requisition_Item rItem = ReqBS.findByReqIDItemCode(id, itemDes);
+        string iCode = rItem.ItemCode;
+        int rId = rItem.RequisitionID;
+        ReqBS.removeRequisitionItem(rId,iCode);
+
+        showAllItems();
+    }
+
+    protected void ReqRow_Updating(object sender, GridViewUpdateEventArgs e)
+    {
+        System.Web.UI.WebControls.TextBox qtyText = (System.Web.UI.WebControls.TextBox)GridView1.Rows[e.RowIndex].FindControl("qtyText");
+        int newQty = Convert.ToInt32(qtyText.Text);
+
+        System.Web.UI.WebControls.Label itemDescLabel = (System.Web.UI.WebControls.Label)GridView1.Rows[e.RowIndex].FindControl("itemDes");
+        string itemDesc = itemDescLabel.Text;
+
+        Requisition_Item item = ReqBS.findByReqIDItemCode(id, itemDesc);
+        string iCode = item.ItemCode;
+        int rId = item.RequisitionID;
+
+        ReqBS.updateRequisitionItem(rId,iCode, newQty);
+
+        GridView1.EditIndex = -1;
+        showAllItems();
+    }
+
+    protected void RowEdit(object sender, GridViewEditEventArgs e)
+    {
+        GridView1.EditIndex = e.NewEditIndex;
+        showAllItems();
+    }
+
+    protected void RowCancelingEdit(object sender, GridViewCancelEditEventArgs e)
+    {
+        GridView1.EditIndex = -1;
+        showAllItems();
+    }
+}
 

11083c736919e7e0f937c3f1b6497a5da150ca75
Branda <e0224946@u.nus.edu>
2018-01-18 15:47:57 +0800

    Update DepartmentInfo

--- StationeryStoreInventorySystem/App_Code/Model.Designer.cs
+++ StationeryStoreInventorySystem/App_Code/Model.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\Users\User\Desktop\Git Directory\AD Project\StationeryStoreInventorySystem\StationeryStoreInventorySystem\App_Code\Model.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\Branda Ling\Documents\ADPrivateGit\StationeryStoreInventorySystem\App_Code\Model.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
--- StationeryStoreInventorySystem/DepartmentListDRep.aspx.cs
+++ StationeryStoreInventorySystem/DepartmentListDRep.aspx.cs
@@ -7,8 +7,59 @@ using System.Web.UI.WebControls;
 
 public partial class DepartmentListDRep : System.Web.UI.Page
 {
+    static string dcode = "BDTD";
+    Employee empActingDHead = DeptBusinessLogic.getEmployeeListForActingDHeadSelected(dcode);
+    Employee empDRep = DeptBusinessLogic.getEmployeeListForDRepSelected(dcode);
     protected void Page_Load(object sender, EventArgs e)
     {
+        Session["deptcode"] = dcode;
+        if (!IsPostBack)
+        {
+            Department dept = DeptBusinessLogic.getDepartByDepCode(dcode);
+            Employee emp = DeptBusinessLogic.getEmployeeByDeptCode(dcode);
+
+            string aheadname = empActingDHead.EmpName;
+            string detpRname = empDRep.EmpName;
+            string dname = dept.DeptName;
+            string contactname = dept.DeptContactName;
+            string telephone = dept.DeptTelephone;
+            string fax = dept.DeptFax;
+            string dheadname = emp.EmpName;
+
+
+
+            lblDeptName.Text = dname;
+            lblContactName.Text = contactname;
+            lblPhone.Text = telephone;
+            lblFax.Text = fax;
+            lblHeadname.Text = dheadname;
+            lblActingDHead.Text = aheadname;
+            lblDeptRep.Text = detpRname;
+
+            //UpdateCollectionPoint
+            string empCollectionname = DeptBusinessLogic.getDepartmentForCollectionPointSelected(dcode);
+            DropDownListCollectionPoint.DataSource = DeptBusinessLogic.getCollectionPointList();
+            DropDownListCollectionPoint.DataTextField = "CollectionPoint1";
+            DropDownListCollectionPoint.DataValueField = "CollectionLocationID";
+            DropDownListCollectionPoint.DataBind();
+            DropDownListCollectionPoint.Items.FindByText(empCollectionname).Selected = true;
+        }
 
     }
+
+    protected void btnUpdate_Click(object sender, EventArgs e)
+    {
+
+        Session["deptcode"] = dcode;
+        int c = Convert.ToInt16(DropDownListCollectionPoint.SelectedValue);
+        DeptBusinessLogic.UpdateCollectionPoint(dcode, c);
+
+        lblMessage.Text = "Update Successfully!";
+
+
+    }
+
+
+
+
 }

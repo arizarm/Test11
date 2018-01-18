f0054e73a50d7b480665d70c6ed4e39f44b3bc85
Ariz Armand <arizar@u.nus.edu>
2018-01-18 14:48:33 +0800

    a

--- StationeryStoreInventorySystem/RequisitionTrendReport.aspx.cs
+++ StationeryStoreInventorySystem/RequisitionTrendReport.aspx.cs
@@ -11,4 +11,69 @@ public partial class RequisitionTrend : System.Web.UI.Page
     {
 
     }
+
+    protected void DurationRadioButtonList_SelectedIndexChanged(object sender, EventArgs e)
+    {
+        int selectedIndex = DurationRadioButtonList.SelectedIndex;
+        switch (selectedIndex)
+        {
+            case 0:
+                FromLabel.Visible = false;
+                FromDropDownList.Visible = false;
+                ToLabel.Visible = false;
+                ToDropDownList.Visible = false;
+                DurationDropDownList.Visible = false;
+                DurationAddButton.Visible = false;
+                break;
+            case 1:
+                FromLabel.Visible = true;
+                FromDropDownList.Visible = true;
+                ToLabel.Visible = true;
+                ToDropDownList.Visible = true;
+                DurationDropDownList.Visible = false;
+                DurationAddButton.Visible = false;
+                break;
+            case 2:
+                FromLabel.Visible = false;
+                FromDropDownList.Visible = false;
+                ToLabel.Visible = false;
+                ToDropDownList.Visible = false;
+                DurationDropDownList.Visible = true;
+                DurationAddButton.Visible = true;
+                break;
+        }
+    }
+
+    protected void CategoryRadioButtonList_SelectedIndexChanged(object sender, EventArgs e)
+    {
+        int selectedIndex = CategoryRadioButtonList.SelectedIndex;
+        switch (selectedIndex)
+        {
+            case 0:
+                CategoryDropDownList.Visible = false;
+                CategoryAddButton.Visible = false;
+                break;
+            case 1:
+                CategoryDropDownList.Visible = true;
+                CategoryAddButton.Visible = true;
+                break;
+        }
+        }
+
+    protected void DepartmentRadioButtonList_SelectedIndexChanged(object sender, EventArgs e)
+    {
+        int selectedIndex = DepartmentRadioButtonList.SelectedIndex;
+        switch (selectedIndex)
+        {
+            case 0:
+                DepartmentDropDownList.Visible = false;
+                DepartmentAddButton.Visible = false;
+                break;
+            case 1:
+                DepartmentDropDownList.Visible = true;
+                DepartmentAddButton.Visible = true;
+                break;
+        }
+
+    }
 }

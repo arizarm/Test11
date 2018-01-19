8f11fabc8aa6953af882ac95929c9d2f47334498
april <aprilwang96@gmail.com>
2018-01-19 16:38:02 +0800

    ,,,,

--- StationeryStoreInventorySystem/Department/RequisitionDetailsEmployee.aspx
+++ StationeryStoreInventorySystem/Department/RequisitionDetailsEmployee.aspx
@@ -12,13 +12,15 @@
         <asp:Label ID="Label5" runat="server" Text="Label"></asp:Label></h2>
     <h3>Requested By:
     <asp:Label ID="Label2" runat="server" Text="Label"></asp:Label>
-    <br /></h3>
+        <br />
+    </h3>
     Requested Date:
     <asp:Label ID="Label3" runat="server" Text="Label"></asp:Label>
     <br />
     <strong>Status:
     <asp:Label ID="Label4" runat="server" Text="Label"></asp:Label>
-    </strong><br />
+    </strong>
+    <br />
     <asp:Label ID="Label9" runat="server" Text="Remarks: "></asp:Label><asp:Label ID="Label8" runat="server" Text="Label" Width="200px"></asp:Label>
     <br />
 
@@ -66,7 +68,7 @@
             <asp:TableCell>
                 <asp:Panel ID="Panel3" runat="server">
                     <asp:GridView ID="GridView1" runat="server" AutoGenerateColumns="false" DataKeyNames="Description" OnRowEditing="RowEdit" OnRowCancelingEdit="RowCancelingEdit" OnRowUpdating="ReqRow_Updating" CssClass="mGrid">
-                        
+
                         <Columns>
                             <asp:TemplateField HeaderText="Item" SortExpression="Description">
                                 <ItemTemplate>
@@ -102,6 +104,30 @@
                                 <EditItemTemplate>
                                     <asp:Button ID="CancelItemEdit" runat="server" Text="Cancel" CommandName="Cancel" CssClass="alert-warning" />
                                 </EditItemTemplate>
+                            </asp:TemplateField>
+                        </Columns>
+                    </asp:GridView>
+
+                    <asp:GridView ID="GridView2" runat="server" AutoGenerateColumns="false" DataKeyNames="Description" OnRowEditing="RowEdit" OnRowCancelingEdit="RowCancelingEdit" OnRowUpdating="ReqRow_Updating" CssClass="mGrid">
+
+                        <Columns>
+                            <asp:TemplateField HeaderText="Item" SortExpression="Description">
+                                <ItemTemplate>
+                                    <asp:Label ID="itemDes" runat="server" Text='<%# Bind("Description") %>'></asp:Label>
+                                </ItemTemplate>
+                            </asp:TemplateField>
+                            <asp:TemplateField HeaderText="Amount" SortExpression="RequestedQty" ItemStyle-HorizontalAlign="Center">
+                                <ItemTemplate>
+                                    <asp:Label ID="Label6" runat="server" Text='<%# Bind("RequestedQty") %>'></asp:Label>
+                                </ItemTemplate>
+                                <EditItemTemplate>
+                                    <asp:TextBox ID="qtyText" runat="server" Text='<%# Bind("RequestedQty") %>' TextMode="Number" Width="60px"></asp:TextBox>
+                                </EditItemTemplate>
+                            </asp:TemplateField>
+                            <asp:TemplateField HeaderText="UOM" SortExpression="UnitOfMeasure">
+                                <ItemTemplate>
+                                    <asp:Label ID="Label7" runat="server" Text='<%# Bind("UnitOfMeasure") %>'></asp:Label>
+                                </ItemTemplate>
                             </asp:TemplateField>
                         </Columns>
                     </asp:GridView>
--- StationeryStoreInventorySystem/Department/RequisitionForm.aspx.cs
+++ StationeryStoreInventorySystem/Department/RequisitionForm.aspx.cs
@@ -11,21 +11,22 @@ using System.Transactions;
 public partial class RequisitionForm : System.Web.UI.Page
 {
     //ReqBS bs = new ReqBS();
-    ArrayList reqItem =new ArrayList();
-    RequestedItem ri;
+    List<RequestedItem> rItem = new List<RequestedItem>();
+    //ArrayList reqItem =new ArrayList();
+    static RequestedItem ri;
     string des;
     protected void Page_Load(object sender, EventArgs e)
     {
         Label1.Text = DateTime.Now.ToLongDateString();
-        if(!IsPostBack)
+        if (!IsPostBack)
         {
             int id = Convert.ToInt32(RequisitionControl.getLastReq()) + 1;
             Label3.Text = "Form:ENGL/" + id;
-            ViewState["list"] = reqItem;
+            ViewState["list"] = rItem;
             DropDownList1.DataSource = RequisitionControl.getItem();
             DropDownList1.DataBind();
         }
-        reqItem = (ArrayList)ViewState["list"];
+        rItem = (List<RequestedItem>)ViewState["list"];
         des = DropDownList1.SelectedItem.ToString();
         Label2.Text = RequisitionControl.getUOM(des);
         Label4.Text = RequisitionControl.getCode(des);
@@ -38,63 +39,108 @@ public partial class RequisitionForm : System.Web.UI.Page
         des = DropDownList1.SelectedItem.ToString();
         int qty = Convert.ToInt32(TextBox4.Text);
 
-        ri = new RequestedItem(Label4.Text, des, qty);
-        reqItem = (ArrayList)ViewState["list"];
-        reqItem.Add(ri);
-        ViewState["list"] = reqItem;
+        if (GridView2.Rows.Count <= 0)
+        {
+            ri = new RequestedItem(Label4.Text, des, qty, Label2.Text);
+            rItem = (List<RequestedItem>)ViewState["list"];
+            rItem.Add(ri);
+            //reqItem.Add(ri);
+            ViewState["list"] = rItem;
+        }
 
-        GridView1.DataSource = reqItem;
-        GridView1.DataBind();
+        else
+        {
+            bool isEqual = false;
+            foreach (GridViewRow row in GridView2.Rows)
+            {
+                System.Web.UI.WebControls.Label labelDes = (System.Web.UI.WebControls.Label)row.FindControl("code");
+                string item = labelDes.Text;
+
+                if (Label4.Text.Equals(item))
+                {
+                    isEqual = true;
+                }
+            }
+            if (isEqual)
+            {
+                Response.Write("<script>alert('Item is already in the form.');</script>");
+            }
+            else
+            {
+                ri = new RequestedItem(Label4.Text, des, qty, Label2.Text);
+                rItem = (List<RequestedItem>)ViewState["list"];
+                rItem.Add(ri);
+                ViewState["list"] = rItem;
+            }
+        }
+        bindGrid();
+    }
+
+    public void bindGrid()
+    {
+        GridView2.DataSource = rItem;
+        GridView2.DataBind();
     }
 
     protected void Submit_Click(object sender, EventArgs e)
     {
-        using (TransactionScope ts = new TransactionScope())
+        if (GridView2.Rows.Count <= 0)
         {
-            StationeryEntities context = new StationeryEntities();
-            Requisition r = new Requisition();
-            r.RequestDate = DateTime.Now;
-            r.Status = "Pending";
-            r.RequestedBy = 1028;
-
-            context.Requisitions.Add(r);
-            context.SaveChanges();
-
-            foreach (GridViewRow row in GridView1.Rows)
-            {
-                Requisition_Item ri = new Requisition_Item();
-                ri.RequisitionID=r.RequisitionID;
-                //string code = row.Cells[0].Text;
-                ri.ItemCode = row.Cells[0].Text;
-                ri.RequestedQty = Convert.ToInt32(row.Cells[2].Text);
-                context.Requisition_Item.Add(ri);
-                context.SaveChanges();
-            }
-            
-            ts.Complete();
+            Response.Write("<script>alert('You have not requested any item yet!');</script>");
         }
-        Response.Write("<script language='javascript'>alert('Requisition Submitted');</script>");
-        Server.Transfer("ReqisitionListDepartment.aspx", true);
-        //Response.Redirect("ReqisitionListDepartment.aspx");
+        else
+        {
+            RequisitionControl.addNewRequisitionItem(rItem);
+        }
+        //Response.Write("<script language='javascript'>alert('Requisition Submitted');</script>");
+        //Server.Transfer("RequisitionListDepartment.aspx", true);
+        Response.Redirect("RequisitionListDepartment.aspx");
 
     }
-}
 
-[Serializable]
-public class RequestedItem
-{
-    private string code;
-    private string description;
-    private int quantity;
-
-    public RequestedItem(string code, string description,int quantity)
+    protected void Delete_Click(object sender, EventArgs e)
     {
-        this.code=code;
-        this.description = description;
-        this.quantity = quantity;
+        GridViewRow row = ((System.Web.UI.WebControls.Button)sender).Parent.Parent as GridViewRow;
+        string itemDes = GridView2.DataKeys[row.RowIndex].Value.ToString();
+
+        RequestedItem i = rItem.Find(r => r.Code.Equals(itemDes));
+
+        rItem = (List<RequestedItem>)ViewState["list"];
+        rItem.Remove(i);
+        ViewState["list"] = rItem;
+
+        bindGrid();
     }
 
-    public string Code { get { return code; } set { code = value; } }
-    public string Description { get { return description; } set { description = value; } }
-    public int Quantity { get { return quantity; } set { quantity = value; } }
+    protected void ReqRow_Updating(object sender, GridViewUpdateEventArgs e)
+    {
+        System.Web.UI.WebControls.TextBox qtyText = (System.Web.UI.WebControls.TextBox)GridView2.Rows[e.RowIndex].FindControl("qtyText");
+        int newQty = Convert.ToInt32(qtyText.Text);
+
+        System.Web.UI.WebControls.Label itemDescLabel = (System.Web.UI.WebControls.Label)GridView2.Rows[e.RowIndex].FindControl("code");
+        string code = itemDescLabel.Text;
+
+        RequestedItem i = rItem.Find(r => r.Code.Equals(code));
+        rItem = (List<RequestedItem>)ViewState["list"];
+
+        i.Quantity = Convert.ToInt32(newQty);
+        rItem[e.RowIndex].Quantity = i.Quantity;
+        ViewState["list"] = rItem;
+
+        GridView2.EditIndex = -1;
+        bindGrid();
+    }
+
+    protected void RowEdit(object sender, GridViewEditEventArgs e)
+    {
+        GridView2.EditIndex = e.NewEditIndex;
+        bindGrid();
+    }
+
+    protected void RowCancelingEdit(object sender, GridViewCancelEditEventArgs e)
+    {
+        GridView2.EditIndex = -1;
+        bindGrid();
+    }
+
 }

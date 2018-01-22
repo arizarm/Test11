cbc39a882dfc6cd960e1efe64e52eedcf6e6ba55
cobsten <cobsten@gmail.com>
2018-01-22 18:53:24 +0800

    update using command for brokers

--- StationeryStoreInventorySystem/App_Code/EFBroker_Item.cs
+++ StationeryStoreInventorySystem/App_Code/EFBroker_Item.cs
@@ -8,80 +8,121 @@ using System.Web;
 /// </summary>
 public class EFBroker_Item
 {
-    StationeryEntities inventoryDB;
     public EFBroker_Item()
     {
-        inventoryDB = new StationeryEntities();
         //
         // TODO: Add constructor logic here
         //
     }
     public void AddItem(Item item)
     {
-        inventoryDB.Items.Add(item);
-        inventoryDB.SaveChanges();
+        using (StationeryEntities inventoryDB = new StationeryEntities())
+        {
+            inventoryDB.Items.Add(item);
+            inventoryDB.SaveChanges();
+        }
         return;
     }
     public Item GetItembyItemCode(string itemCode)
     {
-        Item result = inventoryDB.Items.Where(x => x.ItemCode == itemCode).FirstOrDefault();
-        return result;
+        using (StationeryEntities inventoryDB = new StationeryEntities())
+        {
+            Item result = inventoryDB.Items.Where(x => x.ItemCode == itemCode).FirstOrDefault();
+            return result;
+        }
     }
     public Item GetItembyDescription(string description)
     {
-        Item result = inventoryDB.Items.Where(x => x.Description.Equals(description)).FirstOrDefault();
+        Item result;
+        using (StationeryEntities inventoryDB = new StationeryEntities())
+        {
+            result = inventoryDB.Items.Where(x => x.Description.Equals(description)).FirstOrDefault();
+        }
         return result;
     }
     public void RemoveItem(string itemCode)
     {
-        Item i = inventoryDB.Items.Where(x => x.ItemCode == itemCode).FirstOrDefault();
-        i.ActiveStatus = "N";
-        inventoryDB.SaveChanges();
+
+        using (StationeryEntities inventoryDB = new StationeryEntities())
+        {
+            Item i = inventoryDB.Items.Where(x => x.ItemCode == itemCode).FirstOrDefault();
+            i.ActiveStatus = "N";
+            inventoryDB.SaveChanges();
+        }
+        return;
     }
     public List<Item> GetItemsbyCategoryID(int categoryID)
     {
-        List<Item> itemList = inventoryDB.Items.Where(x=> x.CategoryID==categoryID).ToList();
-        return itemList;
+        List<Item> itemList;
+        using (StationeryEntities inventoryDB = new StationeryEntities())
+        {
+            itemList = inventoryDB.Items.Where(x => x.CategoryID == categoryID).ToList();
+            return itemList;
+        }
     }
     public List<Item> GetItemList()
     {
-        List<Item> itemList = inventoryDB.Items.ToList();
+        List<Item> itemList;
+        using (StationeryEntities inventoryDB = new StationeryEntities())
+        {
+            itemList = inventoryDB.Items.ToList();
+        }
         return itemList;
     }
     public List<Item> GetActiveItemList()
     {
-        List<Item> itemList = inventoryDB.Items.Where(x=> x.ActiveStatus=="Y").ToList();
+        List<Item> itemList;
+        using (StationeryEntities inventoryDB = new StationeryEntities())
+        {
+            itemList = inventoryDB.Items.Where(x => x.ActiveStatus == "Y").ToList();
+        }
         return itemList;
     }
     public List<Item> GetCatalogueList()
     {
-        List<Item> catalogue =
+        List<Item> catalogue;
+        using (StationeryEntities inventoryDB = new StationeryEntities())
+        {
+            catalogue =
             inventoryDB.Items
             .Include("Category")
             .Where(db => db.ActiveStatus == "Y")
             .ToList();
+        }
         return catalogue;
     }
     public List<string> GetDistinctUOMList()
     {
-        List<string> uom = inventoryDB.Items.Select(x => x.UnitOfMeasure).Distinct().ToList();
+        List<string> uom;
+        using (StationeryEntities inventoryDB = new StationeryEntities())
+        {
+            uom = inventoryDB.Items.Select(x => x.UnitOfMeasure).Distinct().ToList();
+        }
         return uom;
     }
     public string GetUnitbyItemCode(string itemCode)
     {
-        string unit = inventoryDB.Items.Where(x => x.ItemCode==itemCode).Select(x=> x.UnitOfMeasure).FirstOrDefault();
+        string unit;
+        using (StationeryEntities inventoryDB = new StationeryEntities())
+        {
+            unit = inventoryDB.Items.Where(x => x.ItemCode == itemCode).Select(x => x.UnitOfMeasure).FirstOrDefault();
+        }
         return unit;
     }
     public void UpdateItem(string itemCode, Category category, string description, int reorderLevel, int reorderQty, string unitOfMeasure, string bin)
     {
-        Item i = GetItembyItemCode(itemCode);
-        i.CategoryID = category.CategoryID;
-        i.Description = description;
-        i.ReorderLevel = reorderLevel;
-        i.ReorderQty = reorderQty;
-        i.UnitOfMeasure = unitOfMeasure;
-        i.Bin = bin;
-        inventoryDB.SaveChanges();
+        Item i;
+        using (StationeryEntities inventoryDB = new StationeryEntities())
+        {
+            i = GetItembyItemCode(itemCode);
+            i.CategoryID = category.CategoryID;
+            i.Description = description;
+            i.ReorderLevel = reorderLevel;
+            i.ReorderQty = reorderQty;
+            i.UnitOfMeasure = unitOfMeasure;
+            i.Bin = bin;
+            inventoryDB.SaveChanges();
+        }
         return;
     }
 }

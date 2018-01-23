a86f784b0633b8d4f86811d1dbb79a790f35f1b7
cobsten <cobsten@gmail.com>
2018-01-23 09:54:48 +0800

    Commit initial brokers

---
+++ StationeryStoreInventorySystem/App_Code/EFBroker_Disbursement_Item.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Summary description for EFBroker_Disbursement_Item
+/// </summary>
+public class EFBroker_Disbursement_Item
+{
+    public EFBroker_Disbursement_Item()
+    {
+        //
+        // TODO: Add constructor logic here
+        //
+    }
+    public List<Disbursement_Item> GetDisbursement_ItemsbyDisbID(int disbID)
+    {
+        List<Disbursement_Item> disbursementDetail = new List<Disbursement_Item>();
+        using (StationeryEntities context = new StationeryEntities())
+        {
+            disbursementDetail = context.Disbursement_Item.Include("Item").Where(x => x.DisbursementID == disbID).ToList();
+        }
+        return disbursementDetail;
+    }
+}
---
+++ StationeryStoreInventorySystem/App_Code/EFBroker_Requisition.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Summary description for EFBroker_Requisition
+/// </summary>
+public class EFBroker_Requisition
+{
+    public EFBroker_Requisition()
+    {
+        //
+        // TODO: Add constructor logic here
+        //
+    }
+    public DateTime GetEarliestReqDateTimebyDisbID(int disbID)
+    {
+        DateTime earliest;
+        using (StationeryEntities context = new StationeryEntities())
+        {
+            earliest = context.Requisitions.Where(x => x.DisbursementID == disbID).OrderBy(x => x.RequestDate).Select(x = x.RequestDate).FirstOrDefault();
+        }
+        return earliest;
+
+    }
+    // alternative
+    public List<DateTime> GetDateTimeListbyDisbID(int disbID)
+    {
+        List<DateTime> dateList;
+        using (StationeryEntities context = new StationeryEntities())
+        {
+            dateList = context.Requisitions.Where(x => x.Disbursement == disbID).OrderBy(x => x.RequestDate).Select(x = x.RequestDate).ToList();
+        }
+        return dateList;
+    }
+    public void AddItemToRequisition(Requisition_Item item)
+    {
+        using (StationeryEntities context = new StationeryEntities())
+        {
+            context.Requisition_Item.Add(item);
+            context.SaveChanges();
+        }
+        return;
+    }
+}

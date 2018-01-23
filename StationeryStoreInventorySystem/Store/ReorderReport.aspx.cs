88566b46c522329348625cca0f1791ee5f9d799e
KiruthikaVenkatesh <gayathri4ashok@gmail.com>
2018-01-23 19:28:27 +0800

    MasterPage

---
+++ StationeryStoreInventorySystem/Store/ReorderReport.aspx.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class ReorderReport : System.Web.UI.Page
+{
+    PurchaseController pCtrlr = new PurchaseController();
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void GenerateBtn_Click(object sender, EventArgs e)
+    {
+        //DateTime startDate = Convert.ToDateTime(txtDate.Text);
+        //DateTime endDate = Convert.ToDateTime(txtEDate.Text);
+        //sDate.Text = startDate.ToShortDateString();
+        //eDate.Text = endDate.ToShortDateString();              
+        //txtLbl.Text= "The following items have fallen below re-order level";
+        //gvPurchasedreoderItem.DataSource=pCtrlr.GenerateReorderReportForPurchasedItems(startDate, endDate);
+        //gvPurchasedreoderItem.DataBind();
+
+        //gvShortfallItems.DataSource = pCtrlr.GenerateShortfallItemsReport();
+        //gvShortfallItems.DataBind();
+
+    }
+}

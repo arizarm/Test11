8d51f87023b2005e1c96355a698bf35898c6b8d2
cobsten <cobsten@gmail.com>
2018-01-24 21:31:43 +0800

    commit javascrip update

--- StationeryStoreInventorySystem/Content/JavaScript.js
+++ StationeryStoreInventorySystem/Content/JavaScript.js
@@ -15,5 +15,16 @@
                dateFormat: 'dd-M-yy'
            }).val()
        });
+       function printDiv() {
+           var divName = "printable";
+           var printContents = document.getElementById(divName).innerHTML;
+           var originalContents = document.body.innerHTML;
+
+           document.body.innerHTML = printContents;
+
+           window.print();
+
+           document.body.innerHTML = originalContents;
+       }
 
 

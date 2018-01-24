0286dd77bcc2c1a8aa43b94eaa1b95139ee59928
Edwin Tan <etedwin123@gmail.com>
2018-01-24 10:46:41 +0800

    added stuff to mobile

---
+++ StationeryStoreInventorySystemMobile/app/src/main/java/com/logic/stationerystoreinventorysystemmobile/DiscrepancyAdhocItemList.java
@@ -0,0 +1,19 @@
+package com.logic.stationerystoreinventorysystemmobile;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ListView;
+
+public class DiscrepancyAdhocItemList extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_discrepancy_adhoc_item_list);
+    }
+
+    protected void finaliseClick(View v){
+
+    }
+}

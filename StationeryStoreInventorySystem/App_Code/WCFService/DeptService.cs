ad1daac7f61a805e5ed0f7f2b079540f59a4a91c
ymsyms <yimonsoe.yms@gmail.com>
2018-01-25 09:29:55 +0800

    Added login

--- StationeryStoreInventorySystem/App_Code/WCFService/DeptService.cs
+++ StationeryStoreInventorySystem/App_Code/WCFService/DeptService.cs
@@ -8,6 +8,23 @@ using System.Text;
 // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service" in code, svc and config file together.
 public class DeptService : IDeptService
 {
+    // Start Login
+    public bool VerifyEmployee(WCFEmployee e)
+    {
+        return EmployeeController.verifyLogin(e.Email, e.Password);
+    }
+
+
+    public WCFEmployee GetEmployeeByEmail(string email)
+    {
+        Employee emp = EmployeeController.GetEmployeeByEmail(email);
+        return WCFEmployee.Make(emp.EmpID, d.DeptName, emp.EmpName, emp.Role, emp.Password
+        , emp.Email, emp.IsTempHead, emp.StartDate.GetValueOrDefault().ToShortDateString()
+        , emp.EndDate.GetValueOrDefault().ToShortDateString());
+    }
+
+    //End Login
+
     //Start Employee Function Part
     public List<WCFEmployee> EmployeeList()
     {
---
+++ StationeryStoreInventorySystemMobile/app/src/main/java/com/logic/stationerystoreinventorysystemmobile/Employee.java
@@ -0,0 +1,8 @@
+package com.logic.stationerystoreinventorysystemmobile;
+
+/**
+ * Created by Yimon Soe on 25/1/2018.
+ */
+
+public class Employee {
+}

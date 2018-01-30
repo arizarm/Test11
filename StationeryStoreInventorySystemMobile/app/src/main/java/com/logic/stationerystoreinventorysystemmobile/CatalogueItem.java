53f1525b83aa9fffe0fa1f86fbf85e715b8b40a7
Edwin Tan <etedwin123@gmail.com>
2018-01-30 16:06:05 +0800

    Restored files and moved PurchaseOrderForm, modified link url in masterpage

--- StationeryStoreInventorySystemMobile/app/src/main/java/com/logic/stationerystoreinventorysystemmobile/CatalogueItem.java
+++ StationeryStoreInventorySystemMobile/app/src/main/java/com/logic/stationerystoreinventorysystemmobile/CatalogueItem.java
@@ -13,25 +13,28 @@ import java.util.HashMap;
  */
 
 public class CatalogueItem extends HashMap<String, String> {
-//    static String ip = "172.17.249.125";
-    static String ip = "172.23.202.59";
+    static String ip = "172.17.249.125";
+//    static String ip = "172.23.202.59";
 //    static String ip = "192.168.1.224";
 
     static String host = "http://"+ ip + "/StationeryStoreInventorySystem/ItemService.svc/";
 
-    public CatalogueItem(String itemCode, String description, String unitOfMeasure, Integer balanceQty, String adjustments){
+    public CatalogueItem(String itemCode, String description, String unitOfMeasure, Integer balanceQty, String adjustments, String bin){
         put("itemCode", itemCode);
         put("description", description);
         put("unitOfMeasure", unitOfMeasure);
         put("balanceQty", balanceQty.toString());
         put("adjustments", adjustments);
+        put("bin", bin);
     }
 
+    //For use in DiscrepancyMonthlyActivity
     public void monthlyActualInput(String actualQty){
         put("correctQty", "N");
         put("actualQty", actualQty.toString());
     }
 
+    //For use in DiscrepancyMonthlyActivity
     public void monthlyCorrectInput(){
         put("correctQty", "Y");
     }
@@ -43,9 +46,9 @@ public class CatalogueItem extends HashMap<String, String> {
             for(int i =0;i<a.length();i++)
             {
                 JSONObject b= a.getJSONObject(i);
-                Integer adjustments = b.getInt("adjustments");
+                Integer adjustments = b.getInt("Adjustments");
                 String adjStr = getAdjustmentString(adjustments);
-                CatalogueItem ci = new CatalogueItem(b.getString("itemCode"), b.getString("description"), b.getString("unitOfMeasure"), b.getInt("balanceQty"), adjStr);
+                CatalogueItem ci = new CatalogueItem(b.getString("ItemCode"), b.getString("Description"), b.getString("UnitOfMeasure"), b.getInt("BalanceQty"), adjStr, b.getString("Bin"));
                 ciList.add(ci);
             }
         }
@@ -61,9 +64,9 @@ public class CatalogueItem extends HashMap<String, String> {
             for(int i =0;i<a.length();i++)
             {
                 JSONObject b= a.getJSONObject(i);
-                Integer adjustments = b.getInt("adjustments");
+                Integer adjustments = b.getInt("Adjustments");
                 String adjStr = getAdjustmentString(adjustments);
-                CatalogueItem ci = new CatalogueItem(b.getString("itemCode"), b.getString("description"), b.getString("unitOfMeasure"), b.getInt("balanceQty"), adjStr);
+                CatalogueItem ci = new CatalogueItem(b.getString("ItemCode"), b.getString("Description"), b.getString("UnitOfMeasure"), b.getInt("BalanceQty"), adjStr, b.getString("Bin"));
                 ciList.add(ci);
             }
         }
@@ -77,9 +80,9 @@ public class CatalogueItem extends HashMap<String, String> {
         CatalogueItem ci = null;
         try {
             JSONObject b = JSONParser.getJSONFromUrl(host+"GetItem/"+itemCode);
-            Integer adjustments = b.getInt("adjustments");
+            Integer adjustments = b.getInt("Adjustments");
             String adjStr = getAdjustmentString(adjustments);
-            ci = new CatalogueItem(b.getString("itemCode"), b.getString("description"), b.getString("unitOfMeasure"), b.getInt("balanceQty"), adjStr);
+            ci = new CatalogueItem(b.getString("ItemCode"), b.getString("Description"), b.getString("UnitOfMeasure"), b.getInt("BalanceQty"), adjStr, b.getString("Bin"));
         }
         catch(Exception e){
             e.printStackTrace();

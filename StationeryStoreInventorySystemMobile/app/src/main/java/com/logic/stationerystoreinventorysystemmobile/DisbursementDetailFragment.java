36b467226f4e4ce0ff84791eaad1ddd2e04e2e72
Mo Mo <khinmomozin@gmail.com>
2018-01-30 11:39:08 +0800

    Retry : Disbursement with awesome adapter *_*

--- StationeryStoreInventorySystemMobile/app/src/main/java/com/logic/stationerystoreinventorysystemmobile/DisbursementDetailFragment.java
+++ StationeryStoreInventorySystemMobile/app/src/main/java/com/logic/stationerystoreinventorysystemmobile/DisbursementDetailFragment.java
@@ -2,6 +2,8 @@ package com.logic.stationerystoreinventorysystemmobile;
 
 
 import android.app.Fragment;
+import android.app.ProgressDialog;
+import android.graphics.Color;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
@@ -13,7 +15,9 @@ import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
@@ -27,23 +31,19 @@ public class DisbursementDetailFragment extends Fragment {
 
     SimpleAdapter sa;
     ListView lv;
-
-    Button button;
-    EditText edtAccessCode;
-    String accessCode;
-
     View view;
+    String accessCode, remark, status, itemCode, itemDesc;
+    int retrievedQty, actQty, reqQty, shortfallQty, discrepancyQty;
+
 
     public DisbursementDetailFragment() {
         // Required empty public constructor
     }
 
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_disbursement_detail, container, false);
-
     }
 
     @Override
@@ -67,6 +67,11 @@ public class DisbursementDetailFragment extends Fragment {
             lv = (ListView) view.findViewById((R.id.lstDisbDetail));
 
             new AsyncTask<Void, Void, List<DisbursementDetailListItems>>() {
+                ProgressDialog progress;
+                @Override
+                protected void onPreExecute() {
+                    progress = ProgressDialog.show(getActivity(), "Loading", "Getting disbursement Detail", true);
+                }
                 @Override
                 protected List<DisbursementDetailListItems> doInBackground(Void... params) {
                     return DisbursementDetailListItems.getDisbursementDetailListItems(disbId);
@@ -75,16 +80,20 @@ public class DisbursementDetailFragment extends Fragment {
                 @Override
                 protected void onPostExecute(List<DisbursementDetailListItems> result) {
 
-                    lv.setAdapter(sa = new SimpleAdapter(getActivity(), result,
-                            R.layout.disbursement_detail_list_row,
-                            new String[]{"ItemCode", "ItemDesc", "ReqQty", "ActualQty", "Remarks"},
-                            new int[]{R.id.itemCodeHidden, R.id.txtIDesc, R.id.txtReqQty, R.id.editTxtActQty, R.id.editTxtRemark}));
-                }
+//                    lv.setAdapter(sa = new SimpleAdapter(getActivity(), result,
+//                            R.layout.disbursement_detail_list_row,
+//                            new String[]{"ItemCode", "ItemDesc", "ReqQty", "ActualQty", "ActualQty", "Remarks"},
+//                            new int[]{R.id.itemCodeHidden, R.id.txtIDesc, R.id.txtReqQty, R.id.editTxtActQty, R.id.retrievedQtyHidden, R.id.editTxtRemark}));
+                    DisbursementAdapter adapter = new DisbursementAdapter(getActivity(), R.layout.disbursement_detail_list_row, result);
+                    lv.setAdapter(adapter);
+                    progress.dismiss();
+
+               }
             }.execute();
 
             try {
 
-                button = view.findViewById(R.id.btnAck);
+                Button button = view.findViewById(R.id.btnAck);
                 button.setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View v) {
@@ -93,16 +102,94 @@ public class DisbursementDetailFragment extends Fragment {
                             @Override
                             protected String doInBackground(Void... params) {
 
-                                edtAccessCode = view.findViewById(R.id.accessCode);
-                                accessCode = edtAccessCode.getText().toString();
-                                final AccessCodeCheck codeCheckObj = new AccessCodeCheck(disbId, accessCode);
+                                //create list for shortfall item to regenerate requisition
+                                List<RequisitionListItem> regenReqList = new ArrayList<RequisitionListItem>();
+                                //crate array list to hold items to be updated
+                                final ArrayList<DisbursementDetailListItems> toBeUpdated = new ArrayList<DisbursementDetailListItems>();
+                                //boolean to validate quantity
+                                boolean actualQtyValidate = true;
+                                //get data from list view
+                                for (int i = 0; i < lv.getCount(); i++) {
+                                    //get view from list view to access controls inside list view
+                                    View vv = lv.getChildAt(i);
 
-                                return AccessCodeCheck.checkAccessCode(codeCheckObj);
+                                    //get textbox values
+                                    EditText edtActQty = vv.findViewById(R.id.editTxtActQty);
+                                    TextView txtReqQty = vv.findViewById(R.id.txtReqQty);
+                                    TextView txtRetrievedQty = view.findViewById(R.id.retrievedQtyHidden);
+                                    EditText edtRemark = vv.findViewById(R.id.editTxtRemark);
+
+                                    //assign textbox values to local variable
+                                    actQty = Integer.parseInt(edtActQty.getText().toString());
+                                    reqQty = Integer.parseInt(txtReqQty.getText().toString());
+                                    retrievedQty = Integer.parseInt(txtRetrievedQty.getText().toString());
+                                    remark = edtRemark.getText().toString();
+
+                                    //check for shortfall item
+                                    if (actQty < reqQty) {
+
+                                        Log.i("ShortfallCheck", "Shortfall item exists");
+
+                                        TextView txtDesc = vv.findViewById(R.id.txtIDesc);
+                                        TextView txtItemCode = view.findViewById(R.id.itemCodeHidden);
+                                        itemDesc = txtDesc.getText().toString();
+                                        itemCode = txtItemCode.getText().toString();
+                                        shortfallQty = reqQty - actQty;
+                                        RequisitionListItem reqItem = new RequisitionListItem(itemCode, itemDesc, String.valueOf(shortfallQty));
+                                        regenReqList.add(reqItem);
+
+                                        Log.i("ShortfallItems", itemCode + itemDesc + String.valueOf(shortfallQty) );
+
+                                        //check for discrepancy
+                                        if (actQty < retrievedQty) {
+                                            discrepancyQty = actQty - retrievedQty;
+                                            DiscrepancyHolder.addDiscrepancy(itemCode, discrepancyQty);
+
+                                            Log.i("Discrepancy Item", itemCode + String.valueOf(discrepancyQty));
+                                        }
+                                    }
+                                    else if (actQty > retrievedQty) {
+                                        //display error for invalid quantity
+                                        actualQtyValidate = false;
+                                    }
+
+                                    //add to disbursement object to be updated in database
+                                    DisbursementDetailListItems detailListItems = new DisbursementDetailListItems(disbId, String.valueOf(actQty), remark);
+                                    toBeUpdated.add(detailListItems);
+                                }
+                                if (actualQtyValidate == false) {
+                                    status = "Actual Quantity cannot be more than requested quantity";
+                                    actualQtyValidate = true;
+                                } else {
+                                    //get textbox values
+                                    EditText edtAccessCode = view.findViewById(R.id.accessCode);
+                                    accessCode = edtAccessCode.getText().toString();
+
+                                    //create object to check access code
+                                    final AccessCodeCheck codeCheckObj = new AccessCodeCheck(disbId, accessCode);
+
+                                    //check access code
+                                    boolean chkR = AccessCodeCheck.checkAccessCode(codeCheckObj);
+
+                                    //check if access code is correct
+                                    if (chkR) {
+
+                                        //save current data to database if access code is correct
+                                        DisbursementDetailListItems.UpdateDisbursement(toBeUpdated);
+                                        status = "Acknowledgement successful!";
+                                    } else {
+                                        //return error if wrong access code
+                                        status = "Wrong Access Code!";
+                                    }
+                                }
+                                return status;
                             }
 
                             @Override
                             protected void onPostExecute(String result) {
                                 Log.i("Result", result);
+                                //display toast message at the end of transaction
+                                customToast(result);
                             }
                         }.execute();
                     }
@@ -113,19 +200,19 @@ public class DisbursementDetailFragment extends Fragment {
         }
     }
 
+    //to get disbursement object from other class
     public void setDisbursementListItems(HashMap<String, String> d) {
         this.d = d;
     }
 
-    protected void btnAckClick(View v) {
-        EditText edtAccessCode = (EditText) v.findViewById(R.id.accessCode);
+    //custom toast message box
+    public void customToast(String message) {
 
-        String accessCode = edtAccessCode.getText().toString();
-        String disbId = d.get("DisbId");
+        Toast toast = Toast.makeText(getActivity(), message,
+                Toast.LENGTH_LONG);
 
-        AccessCodeCheck codeCheckObj = new AccessCodeCheck(disbId, accessCode);
-
-        String s = AccessCodeCheck.checkAccessCode(codeCheckObj);
+        TextView toastMessage = (TextView) toast.getView().findViewById(android.R.id.message);
+        toastMessage.setTextColor(Color.RED);
+        toast.show();
     }
-
 }
